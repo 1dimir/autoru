2271e247a12cde1e698c082d57a245b8c5633b0c
Dmitry Sorokin <a@dimir.name>
2016-08-30 22:35:02 +0300

    card page parsing

--- parser.py
+++ parser.py
@@ -1,7 +1,18 @@
-from io import StringIO
+# -*- coding: utf-8 -*-
+
+import codecs
 from lxml import etree
 from lxml.etree import HTMLParser
 
+
+
+def get_value(tree, xpath):
+	result = tree.xpath(xpath)
+	if result:
+		return result[0].strip()
+	else:
+		return None
+	
 
 class SearchItem(object):
 	
@@ -15,29 +26,28 @@ class SearchItem(object):
 			self.parse(tree)
 		
 	
-	def _get_value(self, tree, xpath):
-		result = tree.xpath(xpath)
-		if result:
-			return result[0].strip()
-		else:
-			return None
-
-
 	def parse(self, tree):
-		self.year = self._get_value(tree, './/div[contains(@class, \'listing-item__year\')]/text()')
-		self.mileage = self._get_value(tree, './/div[contains(@class, \'listing-item__km\')]/text()')
-		self.description = self._get_value(tree, './/div[contains(@class, \'listing-item__description\')]/text()')
-		self.price = self._get_value(tree, './/div[contains(@class, \'listing-item__price\')]/text()')
-		self.currency = self._get_value(tree, './/div[contains(@class, \'listing-item__price\')]/div[contains(@class, \'i-currency-symbol__seo-currency\')]/text()')
-		self.url = self._get_value(tree, './/a[contains(@class, \'listing-item__link\')]/@href')
-		self.model = self._get_value(tree, './/a[contains(@class, \'listing-item__link\')]/text()') + ' ' + self._get_value(tree, './/span[contains(@class, \'listing-item__avtokod\')]/text()')
+		self.year = get_value(tree, './/div[contains(@class, \'listing-item__year\')]/text()')
+		self.mileage = get_value(tree, './/div[contains(@class, \'listing-item__km\')]/text()')
+		self.description = get_value(tree, './/div[contains(@class, \'listing-item__description\')]/text()')
+		self.price = get_value(tree, './/div[contains(@class, \'listing-item__price\')]/text()')
+		self.currency = get_value(tree, './/div[contains(@class, \'listing-item__price\')]'
+			'/div[contains(@class, \'i-currency-symbol__seo-currency\')]/text()')
+		self.url = get_value(tree, './/a[contains(@class, \'listing-item__link\')]/@href')
+		self.model = get_value(tree, './/a[contains(@class, \'listing-item__link\')]/text()') + \
+			' ' + \
+			get_value(tree, './/span[contains(@class, \'listing-item__avtokod\')]/text()')
 	
 class SearchList(object):
 	
 	def __init__(self, tree=None):
 		if not tree is None:
-			self.items = [SearchItem(x) for x in tree.xpath('//tbody[contains(@class, \'listing-item\') and @data-bem]')]
-			self.pages = [x for x in tree.xpath('//span[contains(@class, \'pager__pages\')]/label/input/@value')]
+			self.items = [
+				SearchItem(x) for x in tree.xpath('//tbody[contains(@class, \'listing-item\')'
+								 'and @data-bem]')
+			]
+			self.pages = [x for x in tree.xpath('//span[contains(@class, \'pager__pages\')]'
+								'/label/input/@value')]
 		else:
 			self.items = []
 			self.pages = []
@@ -51,27 +61,26 @@ class SaleItem(object):
 			self.parse(tree)
 		
 	def parse(self, tree):
-		self.properties = ItemProperties(tree)
-
-
-class ItemProperties(object):
-	
-	def __init__(self, tree=None):
-		pass
-
-	def parse(self, tree):
+		self.is_sold = len(tree.xpath('//div[contains(@class, \'card__sold-message\')]')) > 0
+		self.price = get_value(tree, '//h4[contains(@class, \'card__price-rur\')]/text()')
+		self.specification_url = get_value(tree, '//a[contains(@class, \'card__info-details\')]/@href')
+		self.seller_comment = get_value(tree, '//div[contains(@class, \'seller-details__text\')]/text()')
 		
-		self.year = self._get_value(tree, './/div[contains(@class, \'listing-item__year\')]/text()')
-		
+		self.year = get_value(tree, u'//dt[text()= \'\u0413\u043e\u0434 \u0432\u044b\u043f\u0443\u0441\u043a\u0430\']/following::text()')
+
+
 
 if __name__ == '__main__':
 	
 	# test = Test()
 	# print test.i, test.j
-	f = open('test_item.html')
+	f = codecs.open(filename='test_item.html', encoding='utf-8')
+	#f = open('test.html')
 		
-	parser = HTMLParser()
+	parser = HTMLParser(encoding='utf-8')
 	tree = etree.parse(f, parser)
 	
-	search_list = SaleItem(tree)
-	print search_list.properties
+	item = SaleItem(tree)
+	print item.year
+	#search_list = SearchList(tree)
+	#print search_list.items[0].price
